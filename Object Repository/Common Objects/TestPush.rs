b845ed1d1e8be7adbf67c7c726d6e387ddd04765
MartyDy <MartyDy@LAPTOP-KAJKOUHG>
2022-11-04 16:13:20 +0800

    Test Push

---
+++ Object Repository/Common Objects/TestPush.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>TestPush</name>
+   <tag></tag>
+   <elementGuidId>4b438100-0d93-47f5-91d8-b26fb400f6a2</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
